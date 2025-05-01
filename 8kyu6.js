5b1dbdb54ebbcef796de86712b64742e59e17848
Linas <ketleriuslinas@gmail.com>
2025-05-01 20:46:34 +0300

    codewars

--- 8kyu6.js
+++ 8kyu6.js
@@ -7,3 +7,52 @@ console.log(filterWords("You're MEAN! timmy!"),"You're awesome! timmy!");
 // var replaceDots = function(str) {
 //   return str.replaceALL(/./, '-');
 // }
+
+function sum(...s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    count += s[i];
+  }
+  return count;
+}
+console.log(sum(1, 2), 3 );
+console.log(sum(5, 7, 9), 21 );
+console.log(sum(12, 1, 1, 1, 1), 16 );
+
+console.log('--------------------')
+reverse = function(array) {
+  let rev = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    rev.push(array[i]);
+
+  }
+  return rev;
+}
+
+console.log( reverse([1,2,3]), [3,2,1] )
+console.log( reverse([1,null,14,"two"]), ["two",14,null,1] )
+
+console.log('--------------------')
+
+function findShort(s){
+  const words = s.split(' ');
+  let word = [];
+  console.log(words);
+  for (let i = 0; i < words.length; i++) {
+    word.push(words[i].length);
+    
+  }
+  console.log(word);
+  
+  return Math.min(...word);
+}
+
+
+function findShort(s){
+  return Math.min(...s.split(" ").map (s => s.length));
+}
+console.log(findShort("turns out random test cases are easier than writing out basic ones"), 3); 
+console.log(findShort("Let's travel abroad shall we"), 2);
+
+console.log('--------------------')
+
